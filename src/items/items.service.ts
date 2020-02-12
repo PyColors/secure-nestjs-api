70f43a6af79be01ad64831d0ac5bb55193f3b95d
Patrice <patweb44@gmail.com>
2020-02-12 21:28:12 +0100

    Setting 3d points endpoint

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -7,24 +7,30 @@ export class ItemsService {
   private readonly items: Items = {
     1: {
       id: 1,
-      name: 'Burger',
-      price: 5.99,
-      description: 'Tasty',
-      image: 'https://cdn.auth0.com/blog/whatabyte/burger-sm.png',
-    },
-    2: {
-      id: 2,
-      name: 'Pizza',
-      price: 2.99,
-      description: 'Cheesy',
-      image: 'https://cdn.auth0.com/blog/whatabyte/pizza-sm.png',
-    },
-    3: {
-      id: 3,
-      name: 'Tea',
-      price: 1.99,
-      description: 'Informative',
-      image: 'https://cdn.auth0.com/blog/whatabyte/tea-sm.png',
+      version: '1.7',
+      octreeDir: 'data',
+      projection: '',
+      points: 6417736,
+      boundingBox: {
+        lx: 297479.04347171177,
+        ly: 6418078.112909423,
+        lz: 36.83869552612305,
+        ux: 300162.04347171177,
+        uy: 6420761.112909423,
+        uz: 2719.838695526123,
+      },
+      tightBoundingBox: {
+        lx: 297479.04347171177,
+        ly: 6418078.112909423,
+        lz: 36.83869552612305,
+        ux: 299871.04347171177,
+        uy: 6420761.112909423,
+        uz: 396.18280029296877,
+      },
+      pointAttributes: ['POSITION_CARTESIAN', 'COLOR_PACKED'],
+      spacing: 23.235462188720704,
+      scale: 0.001,
+      hierarchyStepSize: 5,
     },
   };
 
